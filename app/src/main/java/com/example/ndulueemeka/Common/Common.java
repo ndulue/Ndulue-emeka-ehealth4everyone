e4bfefc4eb30ddc2c705165cabadbf7618df224c
ndulue <mail2emekandulue@gmail.com>
2020-08-07 18:39:24 +0100

    Create Common class
    To hold the base url and api key

---
+++ app/src/main/java/com/example/ndulueemeka/Common/Common.java
@@ -0,0 +1,8 @@
+package com.example.ndulueemeka.Common;
+
+public class Common {
+
+    public static final String BASE_URL = "https://run.mocky.io/v3/b4cdeed3-327b-4591-9b06-aaf043e65497";
+
+    public static final String apikey = "";
+}
