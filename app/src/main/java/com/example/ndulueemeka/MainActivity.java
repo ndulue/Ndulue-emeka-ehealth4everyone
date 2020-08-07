97bfed95cb3935d2b2fd04792ac84890a299bb9c
ndulue <mail2emekandulue@gmail.com>
2020-08-07 22:11:55 +0100

    testing the mainActivity to see my result

--- app/src/main/java/com/example/ndulueemeka/MainActivity.java
+++ app/src/main/java/com/example/ndulueemeka/MainActivity.java
@@ -1,14 +1,93 @@
 package com.example.ndulueemeka;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+//import android.app.AlertDialog;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.ndulueemeka.Adapter.FilterAdapter;
+import com.example.ndulueemeka.Common.Common;
+import com.example.ndulueemeka.Model.FilterModel;
+import com.example.ndulueemeka.Retrofit.IMYEMEKA;
+import com.example.ndulueemeka.Retrofit.RetrofitClient;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    AlertDialog dialog;
+    FilterAdapter adapter;
+    Retrofit retrofit;
+    IMYEMEKA myemeka;
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        init();
+        initView();
+        getConPrice();
     }
+
+    private void initView() {
+        ButterKnife.bind(this);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        //recyclerview_order.setLayoutManager(linearLayoutManager);
+    }
+
+    private void init() {
+        myemeka = RetrofitClient.getInstance(Common.BASE_URL).create(IMYEMEKA.class);
+        //dialog = new SpotsDialog.Builder().setContext(MainActivity.this).setCancelable(false).build();
+    }
+
+    private void getConPrice() {
+        retrofit = new Retrofit.Builder()
+                .baseUrl("https://run.mocky.io/v3/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        myemeka = retrofit.create(IMYEMEKA.class);
+        Call<List<FilterModel>> filterModelCall = myemeka.geList();
+        filterModelCall.enqueue(new Callback<List<FilterModel>>() {
+            @Override
+            public void onResponse(Call<List<FilterModel>> call, Response<List<FilterModel>> response) {
+                if (response.isSuccessful() ||  response.code() == 200){
+                    //String result = String.valueOf(response.body());
+                    List<FilterModel> filterModels =  response.body();
+
+                    for (FilterModel filterModel : filterModels){
+                        String end_date = String.valueOf(filterModel.getEndYear());
+                        Toast.makeText(MainActivity.this, " "+end_date, Toast.LENGTH_LONG).show();
+                    }
+
+                }else{
+                    Toast.makeText(MainActivity.this, "UnSuccessfull", Toast.LENGTH_LONG).show();
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<List<FilterModel>> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "eeeeeeeeeeee" + t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/example/ndulueemeka/Retrofit/IMYEMEKA.java
+++ app/src/main/java/com/example/ndulueemeka/Retrofit/IMYEMEKA.java
@@ -2,11 +2,13 @@ package com.example.ndulueemeka.Retrofit;
 
 import com.example.ndulueemeka.Model.FilterModel;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface IMYEMEKA {
 
-    @GET()
-    Call<FilterModel> getWallet();
+    @GET("b4cdeed3-327b-4591-9b06-aaf043e65497")
+    Call<List<FilterModel>> geList();
 }
